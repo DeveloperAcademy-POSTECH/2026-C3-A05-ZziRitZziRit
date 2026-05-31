5fbd009edac4086e1e3a2e20201dd6e448f8fcfb
sun <sts07190@naver.com>
2026-05-31 23:20:27 +0900

    [setting] 프로젝트 생성
    파일 구조 정리

---
+++ Mayoty/Mayoty/App/MayotyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MayotyApp.swift
+//  Mayoty
+//
+//  Created by sun on 5/31/26.
+//
+
+import SwiftUI
+
+@main
+struct MayotyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
