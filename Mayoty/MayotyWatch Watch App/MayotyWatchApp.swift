5dcb8625350ee5fdf7931e9f2644db9b413647a7
sun <sts07190@naver.com>
2026-05-31 23:23:43 +0900

    [setting] watchOS 프로젝트 생성

---
+++ Mayoty/MayotyWatch Watch App/MayotyWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MayotyWatchApp.swift
+//  MayotyWatch Watch App
+//
+//  Created by sun on 5/31/26.
+//
+
+import SwiftUI
+
+@main
+struct MayotyWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
