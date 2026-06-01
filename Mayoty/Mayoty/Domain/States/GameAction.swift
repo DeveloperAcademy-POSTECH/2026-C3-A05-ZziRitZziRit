4dc76d94afc0ccd9360724ebbb82de5b14cdb77a
sun <sts07190@naver.com>
2026-06-01 23:44:57 +0900

    [feat/#7] GameAction 정의

---
+++ Mayoty/Mayoty/Domain/States/GameAction.swift
@@ -0,0 +1,23 @@
+//
+//  GameAction.swift
+//  Mayoty
+//
+//  Created by sun on 6/1/26.
+//
+
+enum GameAction {
+    case startGame
+    case rolesAssigned
+
+    case mafiaSelected(target: Player)
+    case policeSelected(target: Player)
+    case doctorSelected(target: Player)
+
+    case discussionEnded
+    case voteSubmitted(voter: Player, target: Player)
+    case voteCompleted
+
+    case finalDefenseEnded
+    case executionCompleted
+    case gameEnded
+}
