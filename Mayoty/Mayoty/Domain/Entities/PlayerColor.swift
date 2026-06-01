eff5734df4ccd04b645ea443a9d07db653e9a377
sun <sts07190@naver.com>
2026-06-01 21:36:00 +0900

    [feat/#1] PlayerColor 정의

---
+++ Mayoty/Mayoty/Domain/Entities/PlayerColor.swift
@@ -0,0 +1,14 @@
+//
+//  PlayerColor.swift
+//  Mayoty
+//
+//  Created by sun on 6/1/26.
+//
+
+enum PlayerColor: String, Codable, CaseIterable {
+    case pink
+    case skyblue
+    case yellow
+    case mint
+    case orange
+}
