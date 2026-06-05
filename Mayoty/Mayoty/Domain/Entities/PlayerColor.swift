d1f304793c9fa7b9a8ecc54ac29c799d274955f4
sun <sts07190@naver.com>
2026-06-05 22:35:25 +0900

    [feat/#31] 조명 색상값 extension 추가

--- Mayoty/Mayoty/Domain/Entities/PlayerColor.swift
+++ Mayoty/Mayoty/Domain/Entities/PlayerColor.swift
@@ -12,3 +12,17 @@ enum PlayerColor: String, Codable, CaseIterable {
     case mint
     case orange
 }
+
+extension PlayerColor {
+    var hue: Double {
+        switch self {
+        case .pink: return 330
+        case .purple: return 270
+        case .yellow: return 60
+        case .mint: return 150
+        case .orange: return 30
+        }
+    }
+    
+    var saturation: Double { 100 }
+}
