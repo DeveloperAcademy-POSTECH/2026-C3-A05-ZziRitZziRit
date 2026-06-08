c8077e8cb0787c12e2d92271e1c5aaf8ced6f2d0
m2kyungmin <dnwndls5828@gmail.com>
2026-06-08 17:20:02 +0900

    [fix/#37] CitizenVictory.swift -> CitizenVictoryView.swift

---
+++ Mayoty/MayotyWatch Watch App/View/CitizenVictoryView.swift
@@ -0,0 +1,31 @@
+//
+//  CitizenVictoryView.swift
+//  MayotyWatch Watch App
+//
+//  Created by 이경민 on 6/5/26.
+//
+
+import SwiftUI
+
+struct CitizenVictoryView: View {
+    var body: some View {
+        MafiaLogoView(baseColor: .bgCitizen) {
+            VStack {
+                Text("시민 승리")
+                    .foregroundStyle(.green)
+                    .font(.title)
+                Button(action:{
+                    print("")
+                }) {
+                    Text("처음으로")
+                        .foregroundStyle(.white)
+                }
+                .foregroundStyle(.btMain)
+            }
+        }
+    }
+}
+
+#Preview {
+    CitizenVictoryView()
+}
